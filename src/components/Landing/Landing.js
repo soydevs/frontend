5dc649bca0348377324d91cc7f96a54a804cb70b
Hemanth R <hemanthrt18@gmail.com>
2021-09-15 23:07:23 +0530

    train

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import './Landing.css'
 
-import train from '../../assets/svg/train.svg'
 
 function Landing() {
     return (
@@ -13,7 +12,6 @@ function Landing() {
                     <div className="landing_input_div">
                         <input type="text" className="landing_input" placeholder="Eg. Edapally"/>
                     </div>
-                    <img src={train} alt="" />
                 </div>
             </div>
         </div>
